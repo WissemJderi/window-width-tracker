0d9d7b989a31c8615dcbc717f35bc9982d538fe7
Wissem Jderi <asqr43641@gmail.com>
2025-02-08 15:13:09 +0100

    refactor WindowTracker to use a named function for resize event listener

--- vite-project/src/WindowTracker.jsx
+++ vite-project/src/WindowTracker.jsx
@@ -4,9 +4,13 @@ export default function WindowTracker() {
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
 
   React.useEffect(() => {
-    window.addEventListener("resize", () => {
+    function watchWindowWidth() {
       setWindowWidth(window.innerWidth);
-    });
+    }
+    window.addEventListener("resize", watchWindowWidth);
+    return function () {
+      window.removeEventListener("resize", watchWindowWidth);
+    };
   }, []);
 
   return <h1>Window width: {windowWidth}</h1>;
